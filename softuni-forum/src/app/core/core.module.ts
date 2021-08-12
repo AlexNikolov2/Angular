56963085db1baf69cc6ce9527678552a8b402945
Aleks Nikolov <tcr.official.world@gmail.com>
2021-08-12 16:13:55 +0300

    commit

---
+++ softuni-forum/src/app/core/core.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent
+  ],
+  providers:[
+    
+  ]
+})
+export class CoreModule { }
---
+++ softuni-forum/src/app/theme/theme.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ITheme } from '../shared/interfaces';
+import { ContentService } from '../content.service';
+
+@Component({
+  selector: 'app-theme',
+  templateUrl: './theme.component.html',
+  styleUrls: ['./theme.component.css']
+})
+export class ThemeComponent {
+
+  themes: ITheme[] | undefined;
+
+  constructor(private contentService: ContentService) {
+    this.fetchThemes();
+   }
+
+  fetchThemes(): void {
+    this.themes = undefined;
+    this.contentService.loadThemes().subscribe(themes => this.themes = themes);
+  }
+
+}
